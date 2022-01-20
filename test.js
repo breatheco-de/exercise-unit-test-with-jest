aabf43c540fe138a584b2dbfc7a579d133228a8b
vitortamberlini <vitor.tamberlini@outlook.com>
2022-01-20 00:28:04 +0000

    Finished project

--- test.js
+++ test.js
@@ -16,13 +16,22 @@ test("One euro should be 1.206 dollars", function(){
      expect(dollars).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
 })
 
-test("One dollar should be 1.206 dollars", function(){
+test("One dollar should be x yen", function(){
     const { fromDollarToYen } = require('./app.js')
 
     const yen = fromDollarToYen(3.5)
 
-    const expected = 3.5 * 1.2; 
+    const expected = (3.5 / 1.2) * 127.9; 
     
-     expect(yen).toBe(expected); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+     expect(yen).toBe(expected);
 })
 
+test("One yen should be x pounds", function(){
+    const { fromYenToPound } = require('./app.js')
+
+    const pounds = fromYenToPound(3.5)
+
+    const expected = (3.5 / 127.9) * 0.8; 
+    
+     expect(pounds).toBe(expected);
+})
