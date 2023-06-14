54539ee5d7938d4d7720d0b2d3a244343f22f323
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-06-14 16:27:11 +0000

    first commit

--- .learn/resets/10-implementing-the-function/app.js
+++ .learn/resets/10-implementing-the-function/app.js
@@ -2,20 +2,8 @@
 const fromEuroToDollar = function(valueInEuro){
     // convert the given valueInEuro to dollars
     let valueInDollar = valueInEuro * 1.2;
-
+    //return the dollar value
     return valueInDollar;
-}
-const fromDollarToYen = function(valueInDollar){
-    let EuroValue = valueInDollar / 1.2
-    let valueInYen = EuroValue * 127.9;
-
-    return valueInYen;
-}
-const fromYenToPound = function(valueInYen){
-    let EuroValue = valueInYen / 127.9
-    let valueInPound = EuroValue * 0.8;
-
-    return valueInPound;
 }
 // this is my function that sums two numbers
 const sum = (a,b) => {
@@ -27,4 +15,5 @@ console.log(sum(7,3))
 
 // export the function to be used on other files
 // (similar to the keyword `export` when using webpack)
-module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+module.exports = { sum, fromEuroToDollar };
+
