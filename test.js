bef5589b94b2c4c67f8059fcc72325200b1d7fa5
Sammantha73 <109299675+Sammantha73@users.noreply.github.com>
2024-04-25 19:55:00 +0200

    Cambiando cosas

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 // Importar la función sum del archivo app.js
-const { sum } = require('./app.js');
-const { fromEuroToDollar } = require('./app.js');
-const { fromYenesTolibras } = require('./app.js');
-const { fromDollarToYen } = require('./app.js');
+const { sum, fromEuroToDollar, fromYenesTolibras, fromDollarToYen } = require('./app.js');
+
 
 // Comienza tu primera prueba
 test('adds 14 + 9 to equal 23', () => {
@@ -21,7 +19,7 @@ test("One euro should be 1.07 dollars", function() {
     const dollars = fromEuroToDollar(3.5);
 
     // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
+    const expected = dollars * 1.07;
 
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
@@ -32,7 +30,7 @@ test("One euro should be 156,5 yenes", function() {
 
     const yenes = fromDollarToYen(3.5);
 
-    const expected = 3.5 * 156.5;
+    const expected = yenes * 156.5;
 
     
     expect(fromDollarToYen(3.5)).toBe(expected); 
@@ -43,7 +41,7 @@ test("One euro should be 0.87 yenes", function() {
 
     const libras = fromYenesTolibras(3.5);
 
-    const expected = 3.5 * 0.87;
+    const expected =libras * 0.87;
 
     
     expect(fromYenesTolibras(3.5)).toBe(expected); 
