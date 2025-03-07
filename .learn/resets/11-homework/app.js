0a0464eb59c6d3b61adb05fb7816d82953ff58fe
Byron Rolle JR <dareal4our@gmail.com>
2025-03-07 23:02:58 +0000

    final

---
+++ .learn/resets/11-homework/app.js
@@ -0,0 +1,19 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// We include fromEuroToDollar here as well because it needs to be exported
+module.exports = { sum, fromEuroToDollar }
